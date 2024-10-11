6d125506df06a9e1afc80b9d02d1056356e9116b
MD Mehedi Hasan <mhnayem79@gmail.com>
2024-10-11 00:48:07 -0600

    first commit

---
+++ js/login.js
@@ -0,0 +1,5 @@
+document.getElementById('login-button').addEventListener('click',function(event){
+    event.preventDefault()
+    let phonenumber=document.getElementById('phone-number').value
+    console.log(phonenumber)
+})
