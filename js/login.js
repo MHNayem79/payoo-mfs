a60e437504418749ce397cec41467c0f85ae19c2
MD Mehedi Hasan <mhnayem79@gmail.com>
2024-10-11 02:42:33 -0600

    added some more

--- js/login.js
+++ js/login.js
@@ -1,5 +1,13 @@
 document.getElementById('login-button').addEventListener('click',function(event){
     event.preventDefault()
     let phonenumber=document.getElementById('phone-number').value
-    console.log(phonenumber)
+    let pinnumber=document.getElementById('pin-number').value
+    console.log(phonenumber,pinnumber)
+    if(phonenumber==='5'&&pinnumber==='1234'){
+        console.log('you are loged in')
+        window.location.href= '\home.html'
+    }
+    else{
+        alert('wrong id or password')
+    }
 })
